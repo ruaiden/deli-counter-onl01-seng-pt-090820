ddb77e1b7842ac86d700139311c0a41a950072cc
Aiden Marques <aiden.marques94@gmail.com>
2020-09-18 02:46:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 def line(deli)
+<<<<<<< HEAD
   if deli.length == 0 
     puts "The line is currently empty."
   else 
@@ -29,3 +30,41 @@ end
   
   
   
+=======
+  
+  if deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    deli.each_with_index { |name, index| puts "#{index}. #{name}"}
+    
+  end
+end
+
+#def line (deli)
+#  string = "The line is currently: "
+ # if deli.length == 0
+  #  puts "The line is currently empty."
+  #else
+   # positions = deli.each.with_index (1) do |name, index| "#{index}. #{name}."
+    #puts  = "The line is currently:" + positions
+     #  end
+  #end
+#end
+
+
+#def take_a_number(katz_deli, name)
+ # new_katz_deli = []
+  #new_katz_deli << name
+  #index = katz_deli.index
+  #katz_deli.each do |line| 
+  #counter +=1
+   #puts "Welcome, #{line}. You are number #{index + 1} in line."
+# end
+#end
+
+def now_serving(dell)
+  take_a_number.each do |person|
+    dell.shift
+end
+end
+>>>>>>> 21060854403b80515d95ee68a1e1dd36d38b02ab
